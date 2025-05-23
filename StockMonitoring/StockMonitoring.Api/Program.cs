22702c7c7a56b7d252cb5c2d4f5f011883a25cef
mikitasharshun <sharshoon321@gmail.com>
2025-05-23 10:31:32 +0200

    add launchSettings.json and StockMonitoring.sln for project configuration

---
+++ StockMonitoring/StockMonitoring.Api/Program.cs
@@ -0,0 +1,26 @@
+using StockMonitoring.Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<IStockService, StockService>();
+builder.Services.AddScoped<IStockService, StockService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
